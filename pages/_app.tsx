4990f798a80f980f791a107b5e3d894d173e7f0e
Tilak Dave <davetilak003@gmail.com>
2022-02-20 23:46:32 +0530

    ganalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import 'styles/global.css';
-
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+declare global { interface Window { gtag: any; } }
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', 'G-LSP50DVBFS', { page_path: url });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
       <ThemeProvider attribute="class">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,17 @@ export default function Document(props) {
         <link href="/static/favicons/site.webmanifest" rel="manifest" />
         <meta content="#ffffff" name="theme-color" />
         <meta content="#ffffff" name="msapplication-TileColor" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LSP50DVBFS"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LSP50DVBFS', { page_path: window.location.pathname });
+            `,
+          }}
+        />
       </Head>
       <body className="bg-slate-50 dark:bg-slate-900 text-slate-50 dark:text-slate-900">
         <Main />
