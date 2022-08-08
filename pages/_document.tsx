fbfe46bc2a9131db5e6a6b7aceb65709d396905d
Tilak Dave <davetilak003@gmail.com>
2022-08-08 23:49:51 +0530

    manifest updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document(props) {
     <Html lang="en">
       <Head>
         <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-        <link rel="icon" type="image/png" href="/static/images/favicon.ico" />
-        <link rel="apple-touch-icon" href="/static/images/favicon.ico" />
+        <link rel="icon" type="image/png" href="/static/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" href="/static/favicons/favicon.ico" />
         <link href="/static/site.webmanifest" rel="manifest" />
         <meta content="#ffffff" name="theme-color" />
         <meta content="#ffffff" name="msapplication-TileColor" />
