0a7ffd656a4203b6e67951c389353599f2cbf9ae
Tilak Dave <davetilak003@gmail.com>
2022-08-20 01:55:26 +0530

    added header footer to proposal

--- layouts/proposals.tsx
+++ layouts/proposals.tsx
@@ -2,8 +2,13 @@ import Container from 'components/Container';
 
 export default function ProposalsLayout({ children }) {
   return (
+    <Container
+    title="Proposal"
+    >
+
       <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <div className="prose dark:prose-dark w-full">{children}</div>
       </article>
+    </Container>
   );
 }
