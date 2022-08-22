f6964cf4a62241781e2328ff1395aa8605ce2c4f
Tilak Dave <davetilak003@gmail.com>
2022-08-22 14:32:43 +0530

    best practise google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,44 @@ import 'styles/global.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import Script from "next/script";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
       <ThemeProvider attribute="class">
         <Script
-        strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-LMLFTJKQSC"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
 
-      <Script id="google-analytics-script" strategy="lazyOnload">
-        {`
+      <Script id="google-analytics-script" strategy="afterInteractive" 
+      dangerouslySetInnerHTML={{
+        __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-LMLFTJKQSC', {
-          page_path: window.location.pathname,
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
           });
-    `}
-      </Script>
+        `,
+      }}
+      />
         <Component {...pageProps} />
       </ThemeProvider>
   );
